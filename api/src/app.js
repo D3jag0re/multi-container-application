27ea395c0bdb4312a291d7d5d24fb3fcb11573c2
Mat B <91279878+D3jag0re@users.noreply.github.com>
2024-12-23 14:25:33 -0500

    main app middleware and output

--- api/src/app.js
+++ api/src/app.js
@@ -3,6 +3,14 @@ const express = require('express'); // Import Express framework
 const app = express();              // Initialize Express app
 const router=require('express').Router() // Create router
 const todo_model = require('./models/todo') // Import model
-const port = 4000;
 
+// Middleware for parsing request bodies
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Use Routes
 app.use(require("./routes/todos"))
+
+// Server Listening
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
